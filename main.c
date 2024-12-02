2a13d8f405623c4380d11816b24c1c4ed7cf9446
Jeh1el66 <zs21027321@estudiantes.uv.mx>
2024-12-02 00:58:44 -0600

    Se implementaron varias funcionalidades al menu

--- main.c
+++ main.c
@@ -306,6 +306,97 @@ void mostrarhistorial(Queue *queue) {
 }
 
 //-------------------------------------------------------------------------------
+// ----------------------------TOP 10------------------------------------------------
+// ----------------------------HEAPSORT----------------------------------
+void heap(Movie arr[],int n, int  i) {
+  int large = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if (left < n && arr[left].rating > arr[large].rating)
+    large = left;
+
+  if (right < n && arr[right].rating > arr[large].rating)
+    large = right;
+
+  if (large!= i) {
+    swap(&arr[i], &arr[large]);
+    heap(arr, n, large);
+  }
+}
+
+void heapSort(Movie arr[], int n) {
+  for (int i = n / 2 - 1; i >= 0; i--)
+      heap(arr, n, i);
+
+  for (int i=n-1; i>0; i--)
+    {
+      swap(&arr[0], &arr[i]);
+      heap(arr, i, 0);
+    }
+}
+
+void showtop10(Movie movie[], int num_movies) {
+  printf("\nTop 10 peliculas:\n");
+  heapSort(movie, num_movies);
+  for (int i = 0; i < 10; i++) {
+    printf("Titulo: %s, Rating: %.2f, Duracion: %d\n", movie[i].title, movie[i].rating, movie[i].time);
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+
+// --------------- Peliculas por estrellas ------------------------------------
+//
+// void countingsortrating(Movie movies[],int num_movies) {
+//   int max = 5;
+//   int min = 1;
+//   int rango = max - min +1;
+//
+//   // contador para la calificacion
+//   int count[rango];
+//   memset(count, 0, sizeof(count));
+//
+//   // Array para almacenar los resultados ordenados
+//   Movie *rs = (Movie *)malloc(num_movies * sizeof(Movie));
+//   if (rs == NULL) {
+//     printf("Error al asignar memoria\n");
+//     return;
+//   }
+//
+//   // contar las veces que aparecen la calificacion
+//   for (int i= 0; i<num_movies; i++) {
+//     count[movies[i].rating - min]++;
+//   }
+//
+//   // calcular las posiciones acumuladas
+//   for (int i=1;i<rango;i++ ) {
+//     count[i] += count[i-1];
+//   }
+//
+//   // construimos de nuevo el array ordenado
+//   for (int i = num_movies -1 ;i>= 0;i--) {
+//     int position = (int)movies[i].rating - min;
+//     rs[count[position]-1] = movies[1];
+//     count[position]--;
+//   }
+//
+//   // copiamos el resultado al arreglo original
+//   for (int i =0; i<num_movies; i++) {
+//     movies[i] = rs[i];
+//   }
+//
+//   free(rs);
+//
+//   printf("\nPeliculas por estrellas:\n");
+//   for (int i = 0; i < num_movies; i++) {
+//     printf("Titulo: %s, Rating: %.2f, Duracion: %d\n", movies[i].title, movies[i].rating, movies[i].time);
+//   }
+//
+// }
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 int main() {
   int nmovies;
@@ -329,11 +420,12 @@ int main() {
     printf("+--------------------------------------------------------------+\n");
     printf("| 1. Mostrar peliculas por genero                              |\n");
     printf("| 2. Mostrar top 10                                            |\n");
-    printf("| 3. Calcular cuantas peliculas puede ver en el tiempo deseado |\n");
-    printf("| 4. Buscar pelicula                                           |\n");
-    printf("| 5. Ver primer pelicula en el historial                       |\n");
-    printf("| 6. Ver todo el historial de peliculas vistas                 |\n");
-    printf("| 7. Salir                                                     |\n");
+    printf("| 3. Películas ordenadas por calificación (1-5 estrellas)    |\n");
+    printf("| 4. Calcular cuantas peliculas puede ver en el tiempo deseado |\n");
+    printf("| 5. Buscar pelicula                                           |\n");
+    printf("| 6. Ver primer pelicula en el historial                       |\n");
+    printf("| 7. Ver todo el historial de peliculas vistas                 |\n");
+    printf("| 8. Salir                                                     |\n");
     printf("+--------------------------------------------------------------+\n");
     printf("Elija la opcion deseada: ");
     scanf("%d", &x);
@@ -346,17 +438,18 @@ int main() {
         break;
       }
       case 2: {
-        //ToDo: Mostrar top 10
-        printf("%s %s %f %d", movies[0].title, movies[0].genre, movies[0].rating,movies[0].time);
-        printf("\n%s %s %f %d", movies[15].title, movies[15].genre, movies[15].rating,movies[15].time);
-
+        showtop10(movies, nmovies);
         break;
       }
       case 3: {
-        //ToDo: Calcular peliculas en t
+        // countingsortrating(movies,nmovies);
         break;
       }
       case 4: {
+        //ToDo: Calcular peliculas en t
+        break;
+      }
+      case 5: {
         char keyword[MAX_TITLE];
         printf("Introduce una palabra clave: ");
         fgets(keyword, sizeof(keyword), stdin);
@@ -399,7 +492,7 @@ int main() {
         }
         break;
       }
-      case 5: {
+      case 6: {
         Movie *firstMovie = dequeue(&movieQueue);
         if (firstMovie != NULL) {
           printf("La primera pelicula en el historial es: %s\n", firstMovie->title);
@@ -410,11 +503,11 @@ int main() {
         }
         break;
       }
-      case 6: {
+      case 7: {
         mostrarhistorial(&movieQueue);
         break;
       }
-      case 7:
+      case 8:
         printf("Saliendo del programa.\n");
       break;
       default:
